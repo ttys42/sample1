d7f28cbdd1c52b1a7e5898feff9bb82fb35e5e56
Chris <Chris@imac.local>
2015-01-25 23:21:42 +0100

    Added project files

---
+++ bugtest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  bugtest
+//
+//  Created by Chris on 23/01/15.
+//  Copyright (c) 2015 sample. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
